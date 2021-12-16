a6fb18bbd7c41f8b1c0f84b1856c6c7863fc8bec
cutieskye <68032402+cutieskye@users.noreply.github.com>
2021-12-16 21:38:37 +0000

    Make an ASP.NET Web API for adding diary entries

---
+++ backend/Data/DiaryDataContext.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata;
+using DiaryApi.Models;
+
+namespace DiaryApi.Data
+{
+    public partial class DiaryDataContext : DbContext
+    {
+        public DiaryDataContext()
+        {
+        }
+
+        public DiaryDataContext(DbContextOptions<DiaryDataContext> options)
+            : base(options)
+        {
+        }
+
+        public virtual DbSet<Entry> Entries { get; set; } = null!;
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            if (!optionsBuilder.IsConfigured)
+            {
+                optionsBuilder.UseSqlServer("Name=ConnectionStrings:DiaryData");
+            }
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Entry>(entity =>
+            {
+                entity.ToTable("Entry");
+
+                entity.Property(e => e.Title).HasMaxLength(100);
+            });
+
+            OnModelCreatingPartial(modelBuilder);
+        }
+
+        partial void OnModelCreatingPartial(ModelBuilder modelBuilder);
+    }
+}
---
+++ backend/Models/Entry.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace DiaryApi.Models
+{
+    public partial class Entry
+    {
+        public int EntryId { get; set; }
+        public int PersonId { get; set; }
+        public string Title { get; set; } = null!;
+        public string Note { get; set; } = null!;
+        public DateTime CreationDate { get; set; }
+        public DateTime ModificationDate { get; set; }
+    }
+}
