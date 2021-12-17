796ce7dfafb7aef6350c68d5dfe817c017ef1c6b
Akmal <akmalmuxiddinov@mail.ru>
2021-12-17 03:03:39 +0100

    Added header

--- frontend/src/components/Header/Header.tsx
+++ frontend/src/components/Header/Header.tsx
@@ -1,7 +1,10 @@
 import React from 'react';
+import logo from '../../static/LargeLogo.png';
+
+import './Header.css';
 
 const Header = () => {
-  return <h1>Diary</h1>;
+  return <img src={logo} alt="Logo" className="logo" />;
 };
 
 export default Header;
