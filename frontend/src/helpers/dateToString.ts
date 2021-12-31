83de29c24f6ec71b19e68ee2a3229eaca69dafe9
Akmal <akmalmuxiddinov@mail.ru>
2021-12-31 22:53:27 +0100

    Fixed year format error
    Now year always in the format "yyyy-mm-dd"

--- frontend/src/helpers/dateToString.ts
+++ frontend/src/helpers/dateToString.ts
@@ -1,8 +1,13 @@
 // Convert Date object to string 'yyyy-mm-dd'
 const dateToString = (date: Date): string => {
-  return `${date.getFullYear().toString()}-${(
-    date.getMonth() + 1
-  ).toString()}-${date.getDate().toString()}`;
+  const year = date.getFullYear().toString();
+  let month = (date.getMonth() + 1).toString();
+  let day = date.getDate().toString();
+
+  if (month.length < 2) month = `0${month}`;
+  if (day.length < 2) day = `0${day}`;
+
+  return `${year}-${month}-${day}`;
 };
 
 export default dateToString;
