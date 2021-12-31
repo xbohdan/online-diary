c01e3883cf047e2eee3453e0268d5fc85e696a98
Akmal <akmalmuxiddinov@mail.ru>
2021-12-31 22:38:18 +0100

    Fixed reload error on server
    Changed BrowserRouter to HashRouter

--- frontend/src/components/App/App.tsx
+++ frontend/src/components/App/App.tsx
@@ -1,16 +1,16 @@
 import React from 'react';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { Routes, Route, HashRouter } from 'react-router-dom';
 import Note from '../$note/Note/Note';
 import NotesCalendar from '../NotesCalendar/NotesCalendar';
 
 const App = () => {
   return (
-    <Router>
+    <HashRouter>
       <Routes>
         <Route path="/" element={<NotesCalendar />} />
         <Route path="/note/:date" element={<Note />} />
       </Routes>
-    </Router>
+    </HashRouter>
   );
 };
 
