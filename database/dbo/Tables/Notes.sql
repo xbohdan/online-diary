08ef4c15f0171a2cb34f80db30b069b2bb78cf52
cutieskye <68032402+cutieskye@users.noreply.github.com>
2022-01-28 19:11:17 +0000

    Add the database schema

---
+++ database/dbo/Tables/Notes.sql
@@ -0,0 +1,12 @@
+﻿CREATE TABLE [dbo].[Notes] (
+    [NoteId]           INT            IDENTITY (1, 1) NOT NULL,
+    [UserId]           NVARCHAR (450) NOT NULL,
+    [Heading]          NVARCHAR (100) NOT NULL,
+    [Content]          NVARCHAR (MAX) NOT NULL,
+    [InitialDate]      DATE           NOT NULL,
+    [ModificationDate] DATE           NULL,
+    CONSTRAINT [PK_Note] PRIMARY KEY CLUSTERED ([NoteId] ASC),
+    CONSTRAINT [FK_Note_AspNetUsers] FOREIGN KEY ([UserId]) REFERENCES [dbo].[AspNetUsers] ([Id]),
+    CONSTRAINT [AK_Note] UNIQUE ([UserId], [InitialDate])
+);
+
