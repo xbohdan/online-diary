ca7350aae8c5fa0fa182fcde6370ac7682515e81
Akmal <akmalmuxiddinov@mail.ru>
2022-02-15 18:46:03 +0100

    Add return type

--- frontend/src/helpers/isEditable.ts
+++ frontend/src/helpers/isEditable.ts
@@ -1,11 +1,11 @@
 export const dayDurationMs = 24 * 60 * 60 * 1000;
 export const today = new Date().setUTCHours(0, 0, 0, 0);
 
-const isYesterdayOrTomorrow = (initialDate: Date) => {
+const isYesterdayOrTomorrow = (initialDate: Date): boolean => {
   return Math.abs(initialDate.getTime() - today) === dayDurationMs;
 };
 
-const isToday = (initialDate: Date) => {
+const isToday = (initialDate: Date): boolean => {
   return initialDate.getTime() === today;
 };
 
